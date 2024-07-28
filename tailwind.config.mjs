3bb9ee15dd34ae9487e02f3e1e0d4b9dd0b3649d
Omar Aguirre <omaaraguirre@hotmail.com>
2024-07-27 19:42:25 -0500

    build: :heavy_plus_sign: add ui fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'dancing': ['Dancing Script Variable', 'cursive'],
+        'quicksand': ['Quicksand Variable', 'sans-serif'],
+      }
+    }
+  },
+  plugins: []
 }
