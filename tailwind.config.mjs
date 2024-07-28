aeed16545fd4565c091ba5e26ed456382e3ef996
Omar Aguirre <omaaraguirre@hotmail.com>
2024-07-27 19:46:01 -0500

    chore: :lipstick: set ui main colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
       fontFamily: {
         'dancing': ['Dancing Script Variable', 'cursive'],
         'quicksand': ['Quicksand Variable', 'sans-serif'],
+      },
+      colors: {
+        'accent': '#DD8B92',
+        'background': '#030303',
       }
     }
-  },
-  plugins: []
+	},
+	plugins: []
 }
