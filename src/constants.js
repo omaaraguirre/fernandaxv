645efb92d943ac81632485a1c889bea3654cbcf4
Omar Aguirre <omaaraguirre@hotmail.com>
2024-08-18 18:42:21 -0500

    fix: :ambulance: fix vercel assets path

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const prefix = process.env.NODE_ENV === 'production' ? '/dannaxv' : ''
+export const prefix = process.env.NODE_ENV === 'production' ? '' : ''
 
 export 
 const parallaxImages = {
